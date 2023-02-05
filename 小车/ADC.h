9190c5bc8facf026da29d0b0909ff182c8136429
李文轩 <118252355+4040kk@users.noreply.github.com>
2023-02-05 18:23:35 +0800

    Delete 小车 directory

--- 小车/ADC.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef __ADC_H
-#define __ADC_H
-#define u8 unsigned char 
-#define u16 unsigned int 
-	void ADC_init();
-u8 ADC_GetNum();
-#endif
--- 小车/UR0_com.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef __UR0_COM_H
-#define __UR0_COM_H
-#define u8 unsigned char 
-void UartInit0(void);
-void SendChar0(unsigned char Char);
-void  SendString0(unsigned char *p);
-void UR0_com__Rountine();
-#endif
